da4fdcb6621fad101f1c171eccdaca27190c8fb9
Diego Figueroa <diegoaaronfc@gmail.com>
2022-03-02 22:20:55 -0600

    Change filename following js standards

---
+++ js/operators_data_types.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// Operators
+var number1 = 7;
+var number2 = 12;
+// Sum
+var operation = number1 + number2;
+console.log("The operation result is: " + operation)
+// Subtraction
+var operation = number1 - number2;
+console.log("The operation result is: " + operation)
+// Multiplication
+var operation = number1 * number2;
+console.log("The operation result is: " + operation)
+// Division
+var operation = number1 / number2;
+console.log("The operation result is: " + operation)
+// Percentage
+var operation = number2 % number1;
+console.log("The operation result is: " + operation)
+
+// Data types
+var entire = 44;
+var text = "Hi, how are you doing?"
+var true_false = true;
+true_false = false;
+
+// Conversion functions
+var false_number = "33.4";
+
+// Converts the object to a number that represents its value
+console.log(Number(false_number) + entire);
+console.log(Number(new Date()));
+// Converts the object to a entire number
+console.log(parseInt(false_number) + entire);
+// Converts the object to a float number
+console.log(parseFloat(false_number) + entire);
+// Converts the object to a String
+console.log(String(entire));
